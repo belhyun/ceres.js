64297ea33d37840b607bc8210b7aa1a3177ac351
belhyun <belhyun@gmail.com>
2014-04-14 13:18:16 +0900

    update

--- src/ceres.js
+++ src/ceres.js
@@ -445,85 +445,6 @@ var ceres  = ceres || {};
   });
 }).call(this,ceres);
 (function($_){
-  $_.N.namespace("ceres.P");
-  var validator = {
-    types: {},
-    isValid: function(types, data){
-      for(var i=0; i<types.length; i++){
-        var checker = this.types[types[i]];
-        if(!checker.isValid.call(checker,data)){
-          return false;
-        }
-      }
-      return true;
-    }
-  };
-
-  var Cmd = function(arg){
-    this.arg = arg;
-  };
-  Cmd.prototype.execute = function(){
-    this.action.call(this, this.arg);
-  };
-  var maker = function(method, arg){
-    var c = function(){
-      Cmd.call(this,arg);
-    };
-    c.prototype = new Cmd;
-    c.prototype.action = method;
-    return new c;
-  };
-  function Macro(){
-    this.cmds = [];
-  }
-  Macro.prototype = new Cmd;
-  Macro.prototype.add = function(cmd){
-    this.cmds.push(cmd);
-  };
-  Macro.prototype.execute = function(){
-    for(var i=0;i<this.cmds.length;i++){
-      this.cmds[i].execute.call(this.cmds[i]);
-    }
-    this.cmds = [];
-  };
-  var cmdPattern = {
-    maker: maker,
-    macro: Macro,
-    cmd: Cmd
-  };
-  var Factory = function(){
-  };
-  Factory.prototype.get = function(base){
-    if (!Function.prototype.bind) {
-      Function.prototype.bind = function (oThis) {
-        if (typeof this !== "function") {
-          throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-        }
-        var aArgs = Array.prototype.slice.call(arguments, 1), 
-            fToBind = this, 
-            fNOP = function () {},
-            fBound = function () {
-              return fToBind.apply(this instanceof fNOP && oThis
-                  ? this
-                  : oThis,
-                  aArgs.concat(Array.prototype.slice.call(arguments)));
-            };
-
-        fNOP.prototype = this.prototype;
-        fBound.prototype = new fNOP();
-
-        return fBound;
-      };
-    }
-    return new (Function.prototype.bind.apply(base,arguments));
-  };
-  $_.B.extend($_.P,{
-    validator: validator,
-    cmdPattern: cmdPattern,
-    factory: new Factory
-  });
-}).call(this,ceres);
-(function($_){
   $_.N.namespace("ceres.S");
   var isEmpty = function(v){
     if(!$_.O.isString(v)){
@@ -699,8 +620,87 @@ var ceres  = ceres || {};
       }
     });
   };
+  if($_.O.isUndefined(Function.prototype.bind)){
+    Function.prototype.bind = function (oThis) {
+      if (typeof this !== "function") {
+        throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+      }
+      var aArgs = Array.prototype.slice.call(arguments, 1), 
+          fToBind = this, 
+          fNOP = function () {},
+          fBound = function () {
+            return fToBind.apply(this instanceof fNOP && oThis
+                ? this
+                : oThis,
+                aArgs.concat(Array.prototype.slice.call(arguments)));
+          };
+
+      fNOP.prototype = this.prototype;
+      fBound.prototype = new fNOP();
+
+      return fBound;
+    };
+  }
   $_.B.extend($_.PROTO,{
     mix:mix
+  });
+}).call(this,ceres);
+(function($_){
+  $_.N.namespace("ceres.P");
+  var validator = {
+    types: {},
+    isValid: function(types, data){
+      for(var i=0; i<types.length; i++){
+        var checker = this.types[types[i]];
+        if(!checker.isValid.call(checker,data)){
+          return false;
+        }
+      }
+      return true;
+    }
+  };
+
+  var Cmd = function(arg){
+    this.arg = arg;
+  };
+  Cmd.prototype.execute = function(){
+    this.action.call(this, this.arg);
+  };
+  var maker = function(method, arg){
+    var c = function(){
+      Cmd.call(this,arg);
+    };
+    c.prototype = new Cmd;
+    c.prototype.action = method;
+    return new c;
+  };
+  function Macro(){
+    this.cmds = [];
+  }
+  Macro.prototype = new Cmd;
+  Macro.prototype.add = function(cmd){
+    this.cmds.push(cmd);
+  };
+  Macro.prototype.execute = function(){
+    for(var i=0;i<this.cmds.length;i++){
+      this.cmds[i].execute.call(this.cmds[i]);
+    }
+    this.cmds = [];
+  };
+  var cmdPattern = {
+    maker: maker,
+    macro: Macro,
+    cmd: Cmd
+  };
+  var Factory = function(){
+  };
+  Factory.prototype.get = function(base){
+    return new (Function.prototype.bind.apply(base,arguments));
+  };
+  $_.B.extend($_.P,{
+    validator: validator,
+    cmdPattern: cmdPattern,
+    factory: new Factory
   });
 }).call(this,ceres);
 (function($_){
