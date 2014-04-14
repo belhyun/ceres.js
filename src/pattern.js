90716e96f472d3c2290df9058a4f32c32174fcfe
belhyun <belhyun@gmail.com>
2014-04-14 13:16:00 +0900

    update

--- src/pattern.js
+++ src/pattern.js
@@ -48,27 +48,6 @@
   var Factory = function(){
   };
   Factory.prototype.get = function(base){
-    if (!Function.prototype.bind) {
-      Function.prototype.bind = function (oThis) {
-        if (typeof this !== "function") {
-          throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-        }
-        var aArgs = Array.prototype.slice.call(arguments, 1), 
-            fToBind = this, 
-            fNOP = function () {},
-            fBound = function () {
-              return fToBind.apply(this instanceof fNOP && oThis
-                  ? this
-                  : oThis,
-                  aArgs.concat(Array.prototype.slice.call(arguments)));
-            };
-
-        fNOP.prototype = this.prototype;
-        fBound.prototype = new fNOP();
-
-        return fBound;
-      };
-    }
     return new (Function.prototype.bind.apply(base,arguments));
   };
   $_.B.extend($_.P,{
